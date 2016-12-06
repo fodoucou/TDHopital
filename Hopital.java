f26af533a95804777f1167747221fea3c7888d6e
fodoucou <doucoure-f@hotmail.fr>
2016-12-06 14:50:18 +0100

    Add files via upload

---
+++ Hopital.java
@@ -0,0 +1,19 @@
+import java.util.Set;
+
+public class Hopital {
+   private Set<String> var1;
+   
+   public void setVar1(Set<String> value) {
+      this.var1 = value;
+   }
+   
+   public Set<String> getVar1() {
+      return this.var1;
+   }
+   
+   public void operationmo() {
+      // TODO implement this operation
+      throw new UnsupportedOperationException("not implemented");
+   }
+   
+   }
